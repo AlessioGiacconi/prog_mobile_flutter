9d582ca040611952f5ada389f3980b48e25c8688
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2023-06-14 12:32:41 +0200

    implementata ricerca in base al luogo

--- lib/screens/search_event.dart
+++ lib/screens/search_event.dart
@@ -12,14 +12,16 @@ class SearchEventScreen extends StatefulWidget {
 }
 
 class _SearchEventScreenState extends State<SearchEventScreen> {
-  
-  final CollectionReference _eventsCollection = FirebaseFirestore.instance.collection('events');
-  final List<Map<String,dynamic>> _eventList = [];
-
+  final CollectionReference _eventsCollection =
+      FirebaseFirestore.instance.collection('events');
+  final List<Map<String, dynamic>> _eventList = [];
+  List<Map<String, dynamic>> _filteredList = [];
+  final TextEditingController _searchTextController = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
+    super.didChangeDependencies();
     getEventList();
   }
 
@@ -37,33 +39,68 @@ class _SearchEventScreenState extends State<SearchEventScreen> {
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            hexStringToColor("#9FCF72"),
-            hexStringToColor("#EAFFDD")
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          alignment: Alignment.topCenter,
-          child: ListView.builder(
-            itemCount: _eventList.length,
-            itemBuilder: (context, index) {
-              return EventCard(_eventList[index]);
-            }
-          ),
-          ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("#9FCF72"),
+          hexStringToColor("#EAFFDD")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: _searchTextController,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Filtra in base al Luogo',
+                    filled: true,
+                    fillColor: hexStringToColor('#EAFFDD'),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black54))),
+                onChanged: searchEvent,
+              ),
+            ),
+            Expanded(
+              child: _filteredList.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _filteredList.length,
+                      itemBuilder: (context, index) {
+                        return EventCard(_filteredList[index]);
+                      })
+                  : ListView.builder(
+                      itemCount: _eventList.length,
+                      itemBuilder: (context, index) {
+                        return EventCard(_eventList[index]);
+                      }),
+            ),
+          ],
         ),
+      ),
     );
   }
 
   Future getEventList() async {
-      QuerySnapshot querySnapshot = await _eventsCollection.get();
-      setState(() {
-        querySnapshot.docs.forEach((element) {
-          Map<String, dynamic>? event = element.data() as Map<String, dynamic>?;
-          _eventList.add(event!);
-        });
+    QuerySnapshot querySnapshot = await _eventsCollection.get();
+    setState(() {
+      querySnapshot.docs.forEach((element) {
+        Map<String, dynamic>? event = element.data() as Map<String, dynamic>?;
+        _eventList.add(event!);
       });
+      //_eventList = List.from(querySnapshot.docs.map((event) => Event.fromSnapshot(event)));
+    });
+  }
+
+  void searchEvent(String query) {
+    final suggestions = _eventList.where((event) {
+      final luogoEvento = event['Luogo'].toLowerCase();
+      final input = query.toLowerCase();
+
+      return luogoEvento.contains(input);
+    }).toList();
+
+    setState(() => _filteredList = suggestions);
   }
 }
-
